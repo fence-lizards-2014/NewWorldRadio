ace8f08c7a3f88d217792bfe199883014a92a91d
Dominick <dominickoddo@gmail.com>
2014-07-04 16:32:34 -0700

    getting back location and years active for artists, sending track names from echo to spotify

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,12 @@ class ApplicationController < ActionController::Base
   def search
     # @track = RSpotify::Track.search(params[:track]).first
     # p @track.album.popularity
- @test = Echowrap.artist_search(:artist_location => "New York")
+ @test = Echowrap.artist_search(:name => params[:track], :bucket => ['hotttnesss', 'familiarity', 'artist_location', 'songs'], :results => 1)
+  @title =  @test.first.songs.first.title
+  @track = RSpotify::Track.search(@title).first
 
-
-
-    # @test = Echowrap.artist_search(:name => "lil wayne")
-    p @test.first.songs
+    @test = Echowrap.artist_search(:artist_location => "New York", :artist_start_year_after => "1960", :bucket => ['artist_location', 'hotttnesss', "years_active"] )
+    p @test
     @bio = Echowrap.artist_biographies(:id => @test.first.id)
     @test.first.location
     render :index
