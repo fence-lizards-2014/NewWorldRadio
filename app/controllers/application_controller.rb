4c8b2f84eb3b883e45c3f11ab92bf614c3ec5995
Dominick <dominickoddo@gmail.com>
2014-07-04 15:59:25 -0700

    echonest artist and track search with location params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def index
-    RSpotify::Track.search('Lotta that').first
+    # RSpotify::Track.search('Lotta that').first
   end
 
   def search
-    @track = RSpotify::Track.search(params[:track]).first
+    # @track = RSpotify::Track.search(params[:track]).first
+    # p @track.album.popularity
+ @test = Echowrap.artist_search(:artist_location => "New York")
+
+
+
+    # @test = Echowrap.artist_search(:name => "lil wayne")
+    p @test.first.songs
+    @bio = Echowrap.artist_biographies(:id => @test.first.id)
+    @test.first.location
     render :index
   end
 end
---
+++ config/initializers/echowrap.rb
@@ -0,0 +1,5 @@
+Echowrap.configure do |config|
+  config.api_key =       'VBKVB8LHBGPC9PG1Y'
+  config.consumer_key =  '46f3c1c6e0693642013463d6c29ce2d6'
+  config.shared_secret = '7S21W5zqRUKbNzRhWIvvLw'
+end
