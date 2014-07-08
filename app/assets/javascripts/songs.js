6e2fff708b9e7b89883f34bbdb9333bc0aa98548
Apprentice <dominickoddo@gmail.com>
2014-07-08 15:54:48 -0700

    no more not sending both parameters any more

--- app/assets/javascripts/songs.js
+++ app/assets/javascripts/songs.js
@@ -9,21 +9,21 @@ function playsong(){
     data: {location: $("#location").val(), time: $('.ival').html()} ,
     dataType: 'json'
   }).done(function(data){
-    console.log("done")
-    playlist_song = data.playlist['song']
-    playlist_id = data.playlist['id']
-    duration = data.playlist['duration'] * 1000
-    var baseUrl = "http://www.youtube.com/v/"
-    var endUrl = "?version=3&autoplay=1"
-    var fullUrl = baseUrl + playlist_id + endUrl
-    $('iframe').attr("src", fullUrl)
-    $('#artist_info li').remove()
-    $('#artist_info ul').append("<li>"+playlist_song+"</li>")
+    console.log("done");
+    playlist_song = data.playlist['song'];
+    playlist_id = data.playlist['id'];
+    duration = data.playlist['duration'] * 1000;
+    var baseUrl = "http://www.youtube.com/v/";
+    var endUrl = "?version=3&autoplay=1";
+    var fullUrl = baseUrl + playlist_id + endUrl;
+    $('iframe').attr("src", fullUrl);
+    $('#artist_info li').remove();
+    $('#artist_info ul').append("<li>"+playlist_song+"</li>");
     setTimeout(function() { playsong(); }, duration)
   })
   .fail(function(data){
     // console.log(data)
-    console.log("fail")
+    console.log("fail");
     console.log(data.responseText)
   })
 }
