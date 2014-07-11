7d0f591e2a0638a99a6350c30ea36e6a96e2840a
Anup <anup-pradhan@hotmail.com>
2014-07-11 07:22:11 -0700

    fixed artist and song name display

--- app/assets/javascripts/songs.js
+++ app/assets/javascripts/songs.js
@@ -38,7 +38,7 @@ function playsong(event){
       var endUrl = "?version=3&autoplay=1";
       var fullUrl = baseUrl + playlist_id + endUrl;
       // $('iframe').attr("src", fullUrl)
-      var player_div = '<iframe id="player" type="text/html" width="300" height="200" src="https://www.youtube.com/embed/'+data.playlist['id']+'?enablejsapi=1?wmode=transparent"frameborder="0"></iframe>'
+      var player_div = '<iframe id="player" type="text/html" width="0" height="0" src="https://www.youtube.com/embed/'+data.playlist['id']+'?enablejsapi=1"frameborder="0"></iframe>'
         $('#artist_info ul').append("<li>"+playlist_song+"</li>");
       $('body').append(player_div);
 
@@ -59,8 +59,8 @@ function playsong(event){
         // $('iframe').css("width", "0px");
         var play_button = '<a href="/play" id="click" style="display:none;">Click Me</a>'
         $('body').append(play_button);
-
-       setTimeout(function(){$("#click").on("click", playMe());}, 3000);
+var player_div = '<iframe id="player" type="text/html" width="0" height="0" src="https://www.youtube.com/embed/'+data.playlist['id']+'?enablejsapi=1"frameborder="0"></iframe>'
+       setTimeout(function(){$("#click").on("click", playMe());}, 1000);
          $("#click").trigger('click');
         // event.target.playVideo();
       }
